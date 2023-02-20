399e4001b57c53bcc0e86c27df7025e79669c0b8
georgesaazan <81086197+georgesaazan@users.noreply.github.com>
2023-02-20 18:10:35 +0100

    Add files via upload

---
+++ v2/adm.m
@@ -0,0 +1,24 @@
+% This functions output the possible values of switching signal given its
+% last value.
+% This function takes an argument i\in [1,...,m] and outputs an array of
+% admissible values.
+% ex: adm(2).
+function a=adm(i)
+if i==1
+    a=[2,3,5];
+elseif i==2
+    a=[1,4,6];
+elseif i==3
+    a=[1,4,7];
+elseif i==4
+    a=[2,3,8];
+elseif i==5
+    a=[1,6,7];
+elseif i==6
+    a=[2,5,8];
+elseif i==7
+    a=[3,5,8];
+elseif i==8
+    a=[4,6,7];
+end
+end
